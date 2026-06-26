8fe39c665b033cc6c5588a1c5cb103ec8cb1dc90
Deyvi10 <deyvi2413@gmail.com>
2026-06-26 11:35:30 -0500

    Actualizando ALLOWED_HOSTS para Render y dominios en produccion

--- produmental_config/settings.py
+++ produmental_config/settings.py
@@ -25,7 +25,9 @@ ALLOWED_HOSTS = [
     'produmetalcm.com', 
     'www.produmetalcm.com'
 ]
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # ==============================================================================
 # 2. DEFINICIÓN DE APLICACIONES Y MIDDLEWARE
 # ==============================================================================
